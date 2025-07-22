717f9c597f9137be83e2cc5199dc923d773405ad
sdffdsgsd <sapayevdoniyorbek30@gmail.com>
2025-07-22 20:54:42 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+    console.log('Service Worker: Installed');
+    e.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          './index.html',
+          './manifest.json',
+          './icon.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
